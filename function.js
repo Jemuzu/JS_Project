26776e838f54eb8f32fdb0f5ca1b53fe33d81eda
James Crawford <jcrawford@nyc.yearup.org>
2019-11-12 14:46:37 -0500

    Added image and on hover effects, added JS.

--- function.js
+++ function.js
@@ -1,1 +1,18 @@
-console.log('My name is James')
+var typeString = ['Munder Difflin  -  Fictional Paper'];
+      var i = 0;
+      var count = 0
+      var selectedText = '';
+      var text = '';
+      (function type() {
+        if (count == typeString.length) {
+          count = 30;
+        }
+        selectedText = typeString[count];
+        text = selectedText.slice(0, ++i);
+        document.getElementById('title').innerHTML = text;
+        if (text.length === selectedText.length) {
+          count++;
+          i = 0;
+        }
+        setTimeout(type, 125);
+      }());
